3e984720daee894f9df19231b3759607aad56c71
Bagaric <joseph@josephit.com>
2018-02-02 13:19:06 +0100

    Re-arranged the component directories

--- app/entry.js
+++ app/entry.js
@@ -1,9 +1,7 @@
 // Load all the JavaScript files
 //import './plugins/jquery-scrollbar/jquery.scrollbar.min.js'
 
-import './plugins/select2/js/select2.full.js'
-import './plugins/bootstrap-timepicker/bootstrap-timepicker.js'
-import './plugins/bootstrap-datepicker/js/bootstrap-datepicker.js'
+import 'bootstrap-timepicker/js/bootstrap-timepicker.js'
 import './plugins/bootstrap-form-wizard/js/jquery.bootstrap.wizard.js'
 //import './plugins/bootstrap/js/bootstrap.min.js'
 
@@ -11,11 +9,11 @@ import './js/scripts.js'
 import './pages/js/pages.min.js'
 
 // Load all the CSS files
-import 'bootstrap-timepicker/bootstrap-timepicker.css'
+import 'bootstrap-timepicker/css/bootstrap-timepicker.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery.scrollbar/jquery.scrollbar.css'
 import 'select2/select2.css'
-import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.css'
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.css'
 
 import './pages/css/pages-icons.css'
 import './pages/css/pages.css'
