61391c06eeff2f634bf7cc24158db7877f8ebf19
Adibas03 <adibas03@ymail.com>
2018-02-02 16:13:00 +0100

    move files around;TODO fix datepicker is not a function

--- app/components/App/App.js
+++ app/components/App/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import SidePanel from '../SidePanel/SidePanel';
 import Header from '../Header/Header';
-import ScheduleWizard from '../ScheduleWizard/ScheduleWizard';
+import ScheduleWizard from '../Common/ScheduleWizard';
 
 class App extends Component {
   render() {
--- app/components/ScheduleWizard/index.js
+++ app/components/ScheduleWizard/index.js
@@ -1,1 +1,5 @@
-export {default} from './ScheduleWizard'
+export {ScheduleWizard} from './ScheduleWizard'
+export {TimeSettings} from './TimeSettings'
+export {InfoSettings} from './InfoSettings'
+export {BountySettings} from './BountySettings'
+export {ConfirmSettings} from './ConfirmSettings'
