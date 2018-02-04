dc6d90c161e0e7b889d02c59ef23415527701747
coderwithattitude <coderwithattitude@gmail.com>
2018-02-04 23:41:23 +0100

    app entry and web3Service

--- app/entry.js
+++ app/entry.js
@@ -1,6 +1,7 @@
 // Load all the JavaScript files
 //import './plugins/jquery-scrollbar/jquery.scrollbar.min.js'
 
+import 'bootstrap-timepicker/js/bootstrap-timepicker.js'
 import './plugins/bootstrap-form-wizard/js/jquery.bootstrap.wizard.js'
 //import './plugins/bootstrap/js/bootstrap.min.js'
 
@@ -8,10 +9,11 @@ import './js/scripts.js'
 import './pages/js/pages.min.js'
 
 // Load all the CSS files
+import 'bootstrap-timepicker/css/bootstrap-timepicker.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'jquery.scrollbar/jquery.scrollbar.css'
 import 'select2/select2.css'
-import 'bootstrap-datepicker/dist/css/bootstrap-datepicker3.css'
+import 'bootstrap-datepicker/dist/css/bootstrap-datepicker.css'
 
 import './pages/css/pages-icons.css'
 import './pages/css/pages.css'
