ed3c99a41b2b2ea20237bad0fe997e450fbd1801
Adibas03 <adibas03@ymail.com>
2018-02-06 10:12:32 +0100

    update awaitingMinng screen

--- app/components/Common/AwaitingMining.js
+++ app/components/Common/AwaitingMining.js
@@ -1,11 +1,55 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+//import PropTypes from 'prop-types';
+import { Router, history } from 'react-router';
+import { PacmanLoader} from 'react-spinners';
+
+
+const loaderConfig = {
+  color:'#21ffff'
+}
 
 class AwaitingMining extends Component {
-  state = {}
+  constructor(props){
+    super(props);
+    this.props = Object.assign(this.props,props);
 
-  componentDidMount() {
+    this.state = {
+      transactionHash:'',
+      newContract: '',
+      destination:'',
+      deploying:true,
+      minning:false,
+    }
 
+  }
+
+  async loadUp (){
+
+
+  }
+
+  next (){
+    const { destination } = this.state;
+    let that = this;
+    const PROPERTIES = ['transactionHash','newContract','destination'];
+    const query = PROPERTIES.reduce((result, name) => {
+      result[name] = that.state[name];
+      return result;
+    }, {});
+
+    Router.push({
+      pathname: destination,
+      query,
+    });
+  }
+
+  async componentDidMount() {
+    let {query:{newContract,transactionHash,destination}} = Router;
+    if((!newContract && !transactionHash) || !destination)
+      history.goBack();
+
+    this.setState(Object.assign(this.state,{newContract:newContract,transactionHash:transactionHash,destination:destination}));
+    await this.loadUp();
   }
 
 render() {
@@ -14,9 +58,10 @@ render() {
     <div id="awaitingMining" className="horizontal-center">
 
       <div className="progress-circle-indeterminate m-t-45"></div>
+      <PacmanLoader {...loaderConfig}/>
       <p className="horizontal-center">Awaiting Mining</p>
 
-      <p className="horizontal-center">Transation Hash: <a href="#">{this.props.address}</a></p>
+      <p className="horizontal-center">Transation Hash: <a href="#">{this.state.transactionHash}</a></p>
 
     </div>
   );
@@ -24,7 +69,9 @@ render() {
 }
 
 AwaitingMining.propTypes = {
-  address: PropTypes.any
+  //contractAddress: PropTypes.any
+  //transactionHash: PropTypes.any
+  //destination: PropTypes.any
 };
 
 export default AwaitingMining;
