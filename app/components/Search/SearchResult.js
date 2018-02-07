b778d2002867080f0ee97bdc99e56f38d0f8f7ea
Bagaric <joseph@josephit.com>
2018-02-07 14:38:09 +0100

    Fetched all transactions

--- app/components/Search/SearchResult.js
+++ app/components/Search/SearchResult.js
@@ -7,7 +7,7 @@ class SearchResult extends Component {
     super(props);
     this.state = {
       txHash: props.txHash,
-      txStatus: props.txStatus
+      txResolved: props.txResolved ? "Executed" : "Scheduled"
     };
   }
 
@@ -15,11 +15,11 @@ class SearchResult extends Component {
     return (
       <div>
         <div className="thumbnail-wrapper d48 circular bg-primary text-black inline m-t-10">
-          <div>{this.state.txStatus[0]}</div>
+          <div>{this.state.txResolved[0]}</div>
         </div>
         <div className="p-l-10 inline p-t-5">
           <h5 className="m-b-5">Transaction <span className="semi-bold result-name">{this.state.txHash}</span></h5>
-          <p className="hint-text">{this.state.txStatus}</p>
+          <p className="hint-text">{this.state.txResolved}</p>
         </div>
       </div>
     )
@@ -28,7 +28,7 @@ class SearchResult extends Component {
 
 SearchResult.propTypes = {
   txHash: PropTypes.string,
-  txStatus: PropTypes.string
+  txResolved: PropTypes.bool
 };
 
 export default SearchResult;
