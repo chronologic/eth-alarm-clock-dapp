4f4954887fc3d9250f36c4238c7b4aec7ddf6ce7
coderwithattitude <coderwithattitude@gmail.com>
2018-02-08 09:09:28 +0100

    mobx added to bounty settings

--- app/components/ScheduleWizard/BountySettings.js
+++ app/components/ScheduleWizard/BountySettings.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
 
+@inject('store')
+@observer
 class BountySettings extends Component {
   state = {}
 
@@ -7,9 +10,18 @@ class BountySettings extends Component {
 
   }
 
-render() {
-  return (
+  constructor (props) {
+    super(props)
+//    this._props = this.props;
 
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange (event) {
+     this.props.onChange(event.target.name,event.target.value)
+   }
+render() {
+  const BountySettings = this.props;
+  return (
     <div id="bountySettings">
 
       <div className="row">
@@ -17,14 +29,14 @@ render() {
 
           <div className="form-group form-group-default">
             <label>Time Bounty</label>
-            <input type="text" placeholder="Enter Time Bounty" className="form-control"></input>
+            <input type="text" placeholder="Enter Time Bounty" value={bountySettings.timeBounty} onChange={this.onChange} className="form-control"></input>
           </div>
 
         </div>
       </div>
 
       <div className="checkbox check-primary">
-        <input type="checkbox" id="checkboxRequireDeposit" defaultChecked />
+        <input type="checkbox" value={bountySettings.depositRequired} onChange={this.onChange} id="checkboxRequireDeposit" defaultChecked />
         <label htmlFor="checkboxRequireDeposit">Require Deposit</label>
       </div>
 
@@ -32,13 +44,17 @@ render() {
         <div className="col-md-4">
           <div className="form-group form-group-default">
             <label>Deposit</label>
-            <input type="text" placeholder="Enter Deposit" className="form-control"></input>
+            <input type="text" value={bountySettings.deposit} onChange={this.onChange} placeholder="Enter Deposit" className="form-control"></input>
           </div>
         </div>
       </div>
 
     </div>
   );
+}
+
+BountySettings.propTypes = {
+  onChange: PropTypes.func.isRequired
 }
 }
 
