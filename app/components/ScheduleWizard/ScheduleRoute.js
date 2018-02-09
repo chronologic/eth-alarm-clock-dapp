6d97cdb9862dc73619014891a7015b3dcb9fc920
Bagaric <joseph@josephit.com>
2018-02-09 12:10:18 +0100

    Updated the schedule wizard nav to match design

--- app/components/ScheduleWizard/ScheduleRoute.js
+++ app/components/ScheduleWizard/ScheduleRoute.js
@@ -6,11 +6,7 @@ export class ScheduleRoute extends Component {
     return (
       <div className="container-fluid padding-25 sm-padding-10">
         <h1 className="view-title">Schedule Transaction</h1>
-        <div className="widget-12 card no-border widget-loader-circle no-margin">
-          <div className="card-body">
-            <ScheduleWizard/>
-          </div>
-        </div>
+          <ScheduleWizard/>
       </div>
     );
   }
