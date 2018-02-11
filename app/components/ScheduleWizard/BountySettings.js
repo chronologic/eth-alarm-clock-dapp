c7b46b58021359a84d07999a83bb2e03e43e64a7
Adibas03 <adibas03@ymail.com>
2018-02-11 18:43:57 +0100

    lint-ready BountySettings

--- app/components/ScheduleWizard/BountySettings.js
+++ app/components/ScheduleWizard/BountySettings.js
@@ -1,56 +1,48 @@
-/* eslint-disable */
-import React, { Component } from 'react';
-import AbstractSetting from '../AbstractSetting';
+import React from 'react';
 import { observer, inject } from 'mobx-react';
+import AbstractSetting from './AbstractSetting';
 
 @inject('scheduleStore')
 @observer
 
 class BountySettings extends AbstractSetting {
-  state = {}
-
-  componentDidMount() {
-
-  }
 
   constructor (props) {
     super(props)
-
+    this.state = {}
   }
-render() {
-  const { scheduleStore } = this.props;
-  return (
-    <div id="bountySettings">
 
-      <div className="row">
-        <div className="col-md-4">
+  componentDidMount() {
+  }
 
-          <div className="form-group form-group-default">
-            <label>Time Bounty</label>
-            <input type="text" placeholder="Enter Time Bounty" value={scheduleStore.timeBounty} onChange={this.onChange('timeBounty')} className="form-control"></input>
-          </div>
+  render() {
+    const { scheduleStore } = this.props;
+    return (
+      <div id="bountySettings">
+        <div className="row">
+          <div className="col-md-4">
 
-        </div>
-      </div>
-
-      <div className="checkbox check-primary">
-        <input type="checkbox" id="checkboxRequireDeposit" defaultChecked />
-        <label htmlFor="checkboxRequireDeposit">Require Deposit</label>
-      </div>
-
-      <div className="row">
-        <div className="col-md-4">
-          <div className="form-group form-group-default">
-            <label>Deposit</label>
-            <input type="text" value={scheduleStore.deposit} onChange={this.onChange('deposit')} placeholder="Enter Deposit" className="form-control"></input>
+            <div className="form-group form-group-default">
+              <label>Time Bounty</label>
+              <input type="text" placeholder="Enter Time Bounty" value={scheduleStore.timeBounty} onChange={this.onChange('timeBounty')} className="form-control"></input>
+            </div>
           </div>
         </div>
+        <div className="checkbox check-primary">
+          <input type="checkbox" id="checkboxRequireDeposit" defaultChecked />
+          <label htmlFor="checkboxRequireDeposit">Require Deposit</label>
+        </div>
+        <div className="row">
+          <div className="col-md-4">
+            <div className="form-group form-group-default">
+              <label>Deposit</label>
+              <input type="text" value={scheduleStore.deposit} onChange={this.onChange('deposit')} placeholder="Enter Deposit" className="form-control"></input>
+            </div>
+          </div>
+        </div>
       </div>
-
-    </div>
-  );
-}
-
+    );
+  }
 }
 
 export default BountySettings;
