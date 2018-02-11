39183645512aa2b6b1d98141caa43c65124445ba
coderwithattitude <coderwithattitude@gmail.com>
2018-02-11 18:40:15 +0100

    removed onChange

--- app/components/ScheduleWizard/TimeSettings/TimeComponent.js
+++ app/components/ScheduleWizard/TimeSettings/TimeComponent.js
@@ -24,7 +24,6 @@ class TimeComponent extends AbstractSetting {
       execWindows: presetExecutionWindows
     };
 
-    this.onChange = this.onChange.bind(this);
     momentDurationFormatSetup(moment);
   }
 
