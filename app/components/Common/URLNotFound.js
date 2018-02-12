b0c7705ef98e7ceeda732fb1f883ac8cad9aabbb
Bagaric <joseph@josephit.com>
2018-02-12 09:38:44 +0100

    Matched the style of the 404 page

--- app/components/Common/URLNotFound.js
+++ app/components/Common/URLNotFound.js
@@ -4,10 +4,13 @@ class URLNotFound extends Component {
   render() {
     return (
       <div className="container-fluid padding-25 sm-padding-10">
-        <h1 className="view-title">Error 404</h1>
         <div className="widget-12 card no-border widget-loader-circle no-margin">
-          <div className="card-body">
-            URL not found
+          <div className="card-body absolute-center">
+            <span>
+              <h1 className="error-number">404</h1>
+              <h2 className="semi-bold">Sorry but we couldn't find this page</h2>
+              <p className="p-b-10">This page you are looking for does not exist. <a href="#">Report this?</a></p>
+            </span>
           </div>
         </div>
       </div>
