9fb55f1c4893e0d80b5a32ccddda3cde35c3db44
Bagaric <joseph@josephit.com>
2018-02-12 19:20:10 +0100

    Added Wallet signing and DAY proving views

--- app/components/TimeNode/TimeNodeMain.js
+++ app/components/TimeNode/TimeNodeMain.js
@@ -5,16 +5,16 @@ import TimeNodeLogs from './TimeNodeLogs';
 class TimeNodeMain extends Component {
   render() {
     return (
-      <div id="timeNodeMain" className="padding-25">
+      <div id="timeNodeMain" className="tab-content">
         <ul className="nav nav-tabs nav-tabs-simple" role="tablist" data-init-reponsive-tabs="dropdownfx">
           <li className="nav-item">
-            <a className="active" data-toggle="tab" role="tab" data-target="#tabStatistics" href="#">Statistics</a>
+            <a className="active px-5 py-3" data-toggle="tab" role="tab" data-target="#tabStatistics" href="#">Statistics</a>
           </li>
           <li className="nav-item">
-            <a href="#" data-toggle="tab" role="tab" data-target="#tabLogs">Logs</a>
+            <a className="px-5 py-3" href="#" data-toggle="tab" role="tab" data-target="#tabLogs">Logs</a>
           </li>
         </ul>
-        <div className="tab-content">
+        <div className="tab-content padding-25">
           <div className="tab-pane active" id="tabStatistics">
             <TimeNodeStatistics/>
           </div>
--- app/components/TimeNode/TimeNodeRoute.js
+++ app/components/TimeNode/TimeNodeRoute.js
@@ -7,13 +7,11 @@ class TimeNodeRoute extends Component {
     return (
       <div className="container-fluid padding-25 sm-padding-10 subsection">
         <h1 className="view-title">TimeNode&nbsp;<span className="view-subtitle">x09s0a9sx09</span></h1>
-        <div>
-          <div className="widget-12 card no-border widget-loader-circle no-margin">
-            <TimeNodeMain/>
-            <div className="p-4">
-              <div className="row">
-                <PoweredByEAC className="col-md-2 mt-2" />
-              </div>
+        <div className="widget-12 card no-border widget-loader-circle no-margin">
+          <TimeNodeMain/>
+          <div className="p-4">
+            <div className="row">
+              <PoweredByEAC className="col-md-2 mt-2" />
             </div>
           </div>
         </div>
