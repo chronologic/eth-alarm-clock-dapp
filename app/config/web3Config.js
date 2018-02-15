ee8a32df9b57c7f556ec0332db7ebe115c9ab25f
Adibas03 <adibas03@ymail.com>
2018-02-15 13:14:03 +0100

    update we3Config

--- app/config/web3Config.js
+++ app/config/web3Config.js
@@ -3,6 +3,15 @@ const Networks =  {
   1: 'Mainnet',
   3: 'Ropsten',
   4: 'Rinkeby',
+  42: 'Kovan'
 }
 
-export default { Networks };
+const Explorers =  {
+  0: '127.0.0.1:7545',//Ganache
+  1: 'https://etherscan.io/',
+  3: 'https://ropsten.etherscan.io/',
+  4: 'https://rinkeby.etherscan.io/',
+  42: 'https://kovan.etherscan.io/'
+}
+
+export { Networks,Explorers };
