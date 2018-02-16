f6de2778f0e22185495a8516c89c5df9296f0bc3
coderwithattitude <coderwithattitude@gmail.com>
2018-02-16 12:44:50 +0100

    added ether units to fields

--- app/components/ScheduleWizard/BountySettings.js
+++ app/components/ScheduleWizard/BountySettings.js
@@ -39,7 +39,7 @@ class BountySettings extends AbstractSetting {
           <div className="col-md-4">
             <div className={"form-group form-group-default required "+(_validations.timeBounty?"":" has-error")}>
               <label>Time Bounty</label>
-              <input type="number" placeholder="Enter Time Bounty" value={scheduleStore.timeBounty} onBlur={this.validate('timeBounty')} onChange={this.onChange('timeBounty') } className="form-control"></input>
+              <input type="number" placeholder="Enter Time Bounty in ETH" value={scheduleStore.timeBounty} onBlur={this.validate('timeBounty')} onChange={this.onChange('timeBounty') } className="form-control"></input>
             </div>
             {!_validations.timeBounty &&
               <label className="error">{_validationsErrors.timeBounty}</label>
@@ -55,7 +55,7 @@ class BountySettings extends AbstractSetting {
             <div className="col-md-4">
               <div className={"form-group form-group-default required "+(_validations.deposit?"":" has-error")}>
                 <label>Deposit</label>
-                <input type="number" value={scheduleStore.deposit} onBlur={this.validate('deposit')} onChange={this.onChange('deposit')} placeholder="Enter Deposit" className="form-control"></input>
+                <input type="number" value={scheduleStore.deposit} onBlur={this.validate('deposit')} onChange={this.onChange('deposit')} placeholder="Enter Deposit in ETH" className="form-control"></input>
               </div>
               {!_validations.timeBounty &&
                 <label className="error">{_validationsErrors.deposit}</label>
