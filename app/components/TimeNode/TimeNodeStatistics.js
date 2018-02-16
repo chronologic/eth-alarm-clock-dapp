7549a393d20625a31c6d7158186539f3aa9883d3
Bagaric <joseph@josephit.com>
2018-02-16 12:18:18 +0100

    Added low DAY token balance notification

--- app/components/TimeNode/TimeNodeStatistics.js
+++ app/components/TimeNode/TimeNodeStatistics.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { inject, observer } from 'mobx-react';
 import PropTypes from 'prop-types';
+import { TIMENODE_STATUS } from '../../stores/TimeNodeStore';
 
 @inject('timeNodeStore')
 @observer
 class TimeNodeStatistics extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+    this.state = {
+      timeNodeDisabled: TIMENODE_STATUS.DISABLED
+    };
 
     this.startTimeNode = this.startTimeNode.bind(this);
     this.stopTimeNode = this.stopTimeNode.bind(this);
@@ -15,14 +19,17 @@ class TimeNodeStatistics extends Component {
   async componentWillMount() {
     await this.props.timeNodeStore.getBalance();
     await this.props.timeNodeStore.getDAYBalance();
+    this.setState({
+      timeNodeDisabled: this.props.timeNodeStore.nodeStatus
+    });
   }
 
   getStopButton() {
-    return <button className="btn btn-danger" onClick={this.stopTimeNode}>Stop</button>;
+    return <button className="btn btn-danger" onClick={this.stopTimeNode} disabled={this.state.timeNodeDisabled}>Stop</button>;
   }
 
   getStartButton() {
-    return <button className="btn btn-primary" onClick={this.startTimeNode}>Start</button>;
+    return <button className="btn btn-primary" onClick={this.startTimeNode} disabled={this.state.timeNodeDisabled}>Start</button>;
   }
 
   startTimeNode() {
@@ -34,10 +41,24 @@ class TimeNodeStatistics extends Component {
   }
 
   render() {
+    let timeNodeStatus = null;
+    if (this.state.timeNodeDisabled) {
+      timeNodeStatus = TIMENODE_STATUS.DISABLED;
+    } else {
+      timeNodeStatus = this.props.timeNodeStore.scanningStarted ? 'running' : 'stopped';
+    }
+
+    const dayTokenError = <div className="alert alert-danger" role="alert">
+      <button className="close" data-dismiss="alert"></button>
+      <strong>Error: </strong>Your DAY token balance is too low. Please make sure you have at least 333 DAY tokens.
+    </div>;
+
     return (
       <div id="timeNodeStatistics">
+        {this.state.timeNodeDisabled ? dayTokenError : null}
+
         <h2 className="py-4">
-          Your TimeNode is currently {this.props.timeNodeStore.scanningStarted ? 'running' : 'stopped'}
+          Your TimeNode is currently {timeNodeStatus}.
           <span className="ml-2">{this.props.timeNodeStore.scanningStarted ? this.getStopButton() : this.getStartButton()}</span>
         </h2>
 
