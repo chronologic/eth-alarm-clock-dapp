442b9b913172bc9768a40c88ac79d1e0b203a75b
Adibas03 <adibas03@ymail.com>
2018-02-19 16:12:59 +0100

    powered by eac logo as svg

--- app/components/Common/PoweredByEAC.js
+++ app/components/Common/PoweredByEAC.js
@@ -5,7 +5,7 @@ class PoweredByEAC extends React.Component {
   render() {
     return (
       <div className={ this.props.className }>
-        <img src="img/powered-by-eac.png" alt="logo" data-src="img/powered-by-eac.png" height="36" />
+        <img src="img/powered-by-eac.svg" alt="logo" data-src="img/powered-by-eac.png" height="36" />
       </div>
     )
   }
