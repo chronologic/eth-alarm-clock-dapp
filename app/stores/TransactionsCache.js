66d74091f2b176604c46d49348feb7e70b1c979b
Adibas03 <adibas03@ymail.com>
2018-02-21 11:54:47 +0100

    more fleshing

--- app/stores/TransactionsCache.js
+++ app/stores/TransactionsCache.js
@@ -12,37 +12,86 @@ export default class TransactionsCache {
     @observable contracts = [];
     @observable transactions = [];
 
-    constructor(eac,startBlock){
+    constructor(eac,startBlock) {
         this._eac = eac;
         this.requestFactoryStartBlock = startBlock;
-        this.startLazy();
+        if (this.requestFactoryStartBlock) {
+            this.startLazy();
+        }
     }
 
-    startLazy(){
+    startLazy () {
         this.running = true;
-        this.fetch
+        //this.fetch
     }
 
-    async getTransactions({ startBlock = this.requestFactoryStartBlock, endBlock = 'latest' },cache = this.cached) {
-        if(cache)
+    get allTransactions () {
+        return this.transactions;
+    }
+
+    async getTransactions ({ startBlock = this.requestFactoryStartBlock, endBlock = 'latest' },cache = this.cached) {
+        
+        if (cache && this.running && this.contracts.length > 0) {
             return this.getCachedTransactions();
+        } else {
 
-        const requestFactory = await this._eac.requestFactory();
+            const requestFactory = await this._eac.requestFactory();
 
-        let requestsCreated = await requestFactory.getRequests(startBlock, endBlock);
+            let requestsCreated = await requestFactory.getRequests(startBlock, endBlock);
+            console.log(requestFactory, requestsCreated)
 
-        requestsCreated.reverse();//Switch to most recent block first
+            requestsCreated.reverse();//Switch to most recent block first
+            const requestAddresses = requestsCreated;
 
-        this.contracts = 
+            requestsCreated = requestsCreated.map(request => this._eac.transactionRequest(request));
 
-        requestsCreated = requestsCreated.map(request => this._eac.transactionRequest(request));
+            if (startBlock == this.requestFactoryStartBlock && endBlock == 'latest') {//cache new values if complete fetch
+                console.log(requestAddresses, requestsCreated)
+                //this.cacheContracts(requestAddresses);
+                //this.cacheTransactions(requestsCreated);
+            }
 
-        return requestsCreated;
+            return requestsCreated;
+        }
     }
 
     getCachedTransactions(){
-
+        return this.allTransactions;
     }
+
+    async getAllTransactions(cached = true) {
+        if (cached){
+            return this.allTransactions;
+        }
+
+        this.allTransactions = await this.getTransactions({});
+
+        for (let transaction of this.allTransactions) {
+            await transaction.fillData();
+            transaction.status = await this.getTxStatus(transaction);
+        }
+    }
+
+    cacheContracts (requestContracts = [], updateType ) {
+        const replace = updateType == 'replace';
+        const append = updateType == 'append';
+        if ( replace || this.contracts.length < requestContracts.length ) {
+            this.contracts = requestContracts;
+        } else if (append) {
+            this.contracts.push(requestContracts);
+        }
+    }
+
+    cacheTransactions(transactions = [], updateType) {
+        const replace = updateType == 'replace';
+        const append = updateType == 'append';
+        if (this.transactions.length < transactions.length || replace) {
+            this.transactions = transactions;
+        } else if (append) {
+            this.transactions.push(transactions);
+        }
+    }
+
 
 
 }
