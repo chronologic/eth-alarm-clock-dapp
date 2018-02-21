b1b5efb15efbeca6fa8137cfa57e6c4dedd893f0
Bagaric <joseph@josephit.com>
2018-02-21 12:54:20 +0100

    Added a new wallet file button + more validation

--- app/components/TimeNode/TimeNodeWallet.js
+++ app/components/TimeNode/TimeNodeWallet.js
@@ -9,10 +9,11 @@ class TimeNodeWallet extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        userUploadedWallet: false
+        userUploadedWallet: false,
+        selectedFile: null
     };
     this.verifyKeystore = this.verifyKeystore.bind(this);
-    this.updateUserUploadedWallet = this.updateUserUploadedWallet.bind(this);
+    this._handleFileUpload = this._handleFileUpload.bind(this);
   }
 
   _handleEnterPress = event => {
@@ -23,7 +24,7 @@ class TimeNodeWallet extends Component {
 
   componentDidMount() {
     this.walletTabRef.addEventListener('keyup', this._handleEnterPress);
-    this.walletFileRef.addEventListener('change', this.updateUserUploadedWallet);
+    this.walletFileRef.addEventListener('change', this._handleFileUpload);
   }
 
   componentWillUnmount() {
@@ -31,27 +32,37 @@ class TimeNodeWallet extends Component {
   }
 
   verifyKeystore() {
-    const file = this.walletFileRef.files[0];
-    const timeNodeStore = this.props.timeNodeStore;
-    const password = timeNodeStore.encrypt(this.passwdRef.value);
+    try {
+      const file = this.walletFileRef.files[0];
+      const timeNodeStore = this.props.timeNodeStore;
+      const password = timeNodeStore.encrypt(this.passwdRef.value);
 
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = async function() {
-        const keystore = timeNodeStore.encrypt(reader.result);
-        if (keystore && password) {
-          await timeNodeStore.startClient(keystore, password);
-        }
-      };
-      reader.readAsText(file, "utf-8");
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = async function() {
+          const keystore = timeNodeStore.encrypt(reader.result);
+          const { matches, msg } = timeNodeStore.checkPasswordMatchesKeystore(keystore, password);
+
+          alert(msg);
+
+          if (matches) {
+            await timeNodeStore.startClient(keystore, password);
+          }
+        };
+        reader.readAsText(file, "utf-8");
+      }
+    } catch (e) {
+      alert("Please select a wallet file.");
     }
   }
 
-  updateUserUploadedWallet() {
+  _handleFileUpload() {
     this.setState({
-      userUploadedWallet: true
+      userUploadedWallet: true,
+      selectedFile: this.walletFileRef.files[0].name
     });
   }
+
 
   render() {
     let passwordField = null;
@@ -70,7 +81,6 @@ class TimeNodeWallet extends Component {
           </div>
         </div>
       </div>;
-      // this.passwdRef.addEventListener('keyup', this._handleEnterPress);
     }
 
     return (
@@ -81,11 +91,18 @@ class TimeNodeWallet extends Component {
         <div className="tab-pane active show padding-25">
           <h2>Select Your Wallet File</h2>
           <p>In order to enable TimeNode functionality please unlock your wallet that contains small amount of ETH necessary for scheduled transactions execution.</p>
-          <p>We support the standard Ethereum keystore wallet file (version 3).</p>
-          <p>If you don&#39;t have the wallet yet, please visit <a href="https://www.mycrypto.com" target="_blank" rel="noopener noreferrer">https://www.mycrypto.com</a> and create a new wallet.</p>
-          <input type="file"
-            className="my-3"
-            ref={(el) => this.walletFileRef = el} />
+          <p>We support the standard Ethereum keystore wallet file (v3).</p>
+          <p>If you don&#39;t have the wallet yet, please visit <a href="https://www.mycrypto.com" target="_blank" rel="noopener noreferrer">https://www.mycrypto.com</a> and create a new one.</p>
+
+          <div className="row my-3">
+            <div className="col-md-6">
+              <label htmlFor="walletFile" className="btn btn-block">Select wallet file</label>
+              <input id="walletFile" type="file" className="hide" ref={(el) => this.walletFileRef = el} />
+            </div>
+            <div className="col-md-6">
+              <p>{this.state.selectedFile}</p>
+            </div>
+          </div>
           {passwordField}
         </div>
 
