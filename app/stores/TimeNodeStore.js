bb9d9cdb0527a92b7c20121455f073a1b398e9f7
Adibas03 <adibas03@ymail.com>
2018-02-28 16:02:12 +0100

    move with the flow

--- app/stores/TimeNodeStore.js
+++ app/stores/TimeNodeStore.js
@@ -192,8 +192,8 @@ export default class TimeNodeStore {
     await this._web3Service.init();
     const web3 = this._web3Service.web3;
 
-    const DAY_TOKEN_ADDRESS = JSON.parse(process.env.DAY_TOKEN_ADDRESS)[this._web3Service.netId];
-    const contract = web3.eth.contract(dayTokenABI).at(DAY_TOKEN_ADDRESS);
+    const dayTokenAddress = JSON.parse(process.env.DAY_TOKEN_ADDRESS)[this._web3Service.netId];
+    const contract = web3.eth.contract(dayTokenABI).at(dayTokenAddress);
     const balanceNum = await Bb.fromCallback((callback) => {
       contract.balanceOf.call(address, callback);
     });
