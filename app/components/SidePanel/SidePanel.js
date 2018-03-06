dedce954d5ed34682f290e946667d4360a76aff1
Bagaric <joseph@josephit.com>
2018-03-06 11:35:50 +0100

    Timenode counter instant update added

--- app/components/SidePanel/SidePanel.js
+++ app/components/SidePanel/SidePanel.js
@@ -5,6 +5,7 @@ import { observer,inject } from 'mobx-react';
 
 @inject('web3Service')
 @inject('keenStore')
+@inject('timeNodeStore')
 @observer
 class SidePanel extends Component {
 
@@ -156,7 +157,7 @@ class SidePanel extends Component {
                     <span className="active-timenodes">Active TimeNodes</span>
                   </div>
                   <div className="col-4 px-0 text-right">
-                    <span className="timenode-count col-6">{this.props.keenStore.activeTimeNodes}</span>
+                    <span className="timenode-count col-6">{this.props.keenStore.activeTimeNodes + this.props.timeNodeStore.scanningStarted ? 1 : 0 }</span>
                   </div>
                 </div>
               </div>
@@ -186,7 +187,8 @@ class SidePanel extends Component {
 SidePanel.propTypes = {
   web3Service: PropTypes.any,
   keenStore: PropTypes.any,
-  location: PropTypes.object.isRequired
+  location: PropTypes.object.isRequired,
+  timeNodeStore: PropTypes.any
 };
 
 export default SidePanel;
