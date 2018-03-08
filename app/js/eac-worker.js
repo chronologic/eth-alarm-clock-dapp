9b93597717c148a1c2eb8b0e36b3dff9247ef74d
Adibas03 <adibas03@ymail.com>
2018-03-08 15:00:10 +0100

    support new feature

--- app/js/eac-worker.js
+++ app/js/eac-worker.js
@@ -31,7 +31,7 @@ class EacWorker {
       scanSpread: options.scan,
       logfile: options.logfile,
       logLevel: options.logLevel,
-      walletFile: JSON.parse(options.keystore.toLowerCase()),
+      walletStore: JSON.parse(options.keystore.toLowerCase()),
       password: options.keystorePassword,
       autostart: options.autostart,
       logger,
@@ -43,8 +43,9 @@ class EacWorker {
 
     this.config.logger = logger;
     this.config.statsdb = statsDB;
+    const addresses = await this.config.wallet.getAddresses();
 
-    this.config.statsdb.initialize(this.config.wallet.getAddresses());
+    this.config.statsdb.initialize(addresses);
     this.alarmClient = new Scanner(
       options.milliseconds,
       this.config
