d22d7623f9f9d0bce8b6ea076984c6bbac17a529
Bagaric <joseph@josephit.com>
2018-03-18 11:27:10 +0100

    Fixed a typo

--- app/components/Common/AwaitingMining.js
+++ app/components/Common/AwaitingMining.js
@@ -131,7 +131,7 @@ class AwaitingMining extends Component {
             </div>
             {this.state.transactionHash &&
               <p className="horizontal-center">
-                Transation Hash: <br />
+                Transcation Hash: <br />
               <a target="_blank" href={explorer + '/tx/' + transactionHash } > {this.state.transactionHash} </a>
               </p>
             }
