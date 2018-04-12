16b3b5d26f6a81ce917e5508dfeaf04df0160d9a
Adibas03 <adibas03@ymail.com>
2018-04-12 15:48:39 +0100

    add support for call2Action

--- app/components/Common/Alert.js
+++ app/components/Common/Alert.js
@@ -12,12 +12,24 @@ class Alert extends Component {
   render() {
     const type = this.props.type || 'danger';
     const close = typeof this.props.close !== 'undefined' ? this.props.close : true;
+    const callToAction = this.props.action || null;
     return (
-      <div className={'alert alert-'+type} role='alert'>
-        { close &&
-          <button className='close' data-dismiss='alert'></button>
-        }
-        <strong> {Titles[type]} </strong>{this.props.msg}
+      <div className={' d-flex flex-nowrap alert alert-'+type} role='alert'>
+        <div className='d-flex flex-wrap flex-strech flex-sm-nowrap justify-content-center justify-content-sm-start align-items-center '>
+          <div className='flex-strech'>
+            <strong> {Titles[type]} </strong>{this.props.msg}
+          </div>
+          {callToAction &&
+            <div className='d-inline-block ml-2' >
+              {callToAction}
+            </div>
+          }
+        </div>
+        <div>
+          {close &&
+            <button className='close' data-dismiss='alert'></button>
+          }
+        </div>
       </div>
     );
   }
