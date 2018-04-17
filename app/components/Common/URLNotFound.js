28266f2a7783c023b4bb408fad3ed66ff1591432
Kenny Meyer <kenny@kennymeyer.net>
2018-04-17 10:28:48 -0400

    Replace single quotes

--- app/components/Common/URLNotFound.js
+++ app/components/Common/URLNotFound.js
@@ -4,13 +4,13 @@ import intl from 'react-intl-universal';
 class URLNotFound extends Component {
   render() {
     return (
-      <div className="container-fluid">
-        <div className="card">
-          <div className="card-body absolute-center">
+      <div className='container-fluid'>
+        <div className='card'>
+          <div className='card-body absolute-center'>
             <span>
-              <h1 className="error-number">404</h1>
-              <h2 className="semi-bold">{intl.get('404.SORRY-NOT-FOUND')}</h2>
-              <p className="p-b-10">
+              <h1 className='error-number'>404</h1>
+              <h2 className='semi-bold'>{intl.get('404.SORRY-NOT-FOUND')}</h2>
+              <p className='p-b-10'>
                 {intl.get('404.PAGE-DOES-NOT-EXIST', { report_url: '#' })}
                 &nbsp;
                 <a href='#'>{intl.get('404.REPORT-THIS')}</a>
