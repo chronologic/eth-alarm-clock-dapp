3b81cb19434423770d458b6292f34da52373bbc3
Bagaric <joseph@josephit.com>
2018-04-20 02:58:34 +0700

    Add variables ganache docker script

--- docker/ganache/startup.sh
+++ docker/ganache/startup.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+MNEMONIC='candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+NETWORK_ID='1001'
+BLOCK_TIME='15'
+
+ # Can be improved, for now it relies on contracts to be executed within 60 sec
 echo "Running a blockchain with automine to deploy contracts faster..."
-timeout 60 node ./build/cli.node.js -i 1001 -m 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+timeout 60 node ./build/cli.node.js -i $NETWORK_ID -m "$MNEMONIC"
 echo "Killed automine ganache."
 
-echo "Running ganache with blockTime of 15 sec"
-node ./build/cli.node.js -i 1001 -b 15 -m 'candy maple cake sugar pudding cream honey rich smooth crumble sweet treat'
+echo "Running ganache with blockTime of ${BLOCK_TIME} sec..."
+node ./build/cli.node.js -i $NETWORK_ID -b $BLOCK_TIME -m "$MNEMONIC"
