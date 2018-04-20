e58f6ee8b0e7d2986fff68c97cc552b232560f98
Bagaric <joseph@josephit.com>
2018-04-20 13:46:46 +0700

    Persist ganache data

--- docker/ganache/startup.sh
+++ docker/ganache/startup.sh
@@ -3,10 +3,12 @@ MNEMONIC='candy maple cake sugar pudding cream honey rich smooth crumble sweet t
 NETWORK_ID='1001'
 BLOCK_TIME='15'
 
+mkdir db
+
  # Can be improved, for now it relies on contracts to be deployed within 60 sec
 echo "Running a blockchain with automine to deploy contracts faster..."
-timeout 60 node ./build/cli.node.js -i $NETWORK_ID -m "$MNEMONIC"
+timeout 360 node ./build/cli.node.js -i $NETWORK_ID -m "$MNEMONIC" --db ./db/
 echo "Killed automine ganache."
 
 echo "Running ganache with blockTime of ${BLOCK_TIME} sec..."
-node ./build/cli.node.js -i $NETWORK_ID -b $BLOCK_TIME -m "$MNEMONIC"
+node ./build/cli.node.js -i $NETWORK_ID -b $BLOCK_TIME -m "$MNEMONIC" --db ./db/
