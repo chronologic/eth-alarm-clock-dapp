657873f90e5c119c04a6e7d36dcb9288fd8b3380
Bagaric <joseph@josephit.com>
2018-05-16 18:36:58 +0200

    Bounty estimator ticks on relevant values

--- app/components/ScheduleWizard/BountySettings/BountyEstimator.js
+++ app/components/ScheduleWizard/BountySettings/BountyEstimator.js
@@ -3,30 +3,40 @@ import PropTypes from 'prop-types';
 
 class BountyEstimator extends Component {
   render() {
+    // Percentages of the total line in color
+    const totalWidthPercent = 100;
+    const redLinePercent = 30;
+    const yellowLinePercent = 20;
+    const greenLinePercent = 48;
+    const tickPercent = 1;
+
+    const redLine = <div className="progress-bar bg-danger" role="progressbar" style={{ width: `${redLinePercent}%` }} />;
+    const halfYellowLine = <div className="progress-bar bg-warning" role="progressbar" style={{ width: `${yellowLinePercent / 2}%` }} />;
+    const greenLine = <div className="progress-bar bg-success" role="progressbar" style={{ width: `${greenLinePercent}%` }} />;
+    const tick = <div className="progress-bar bg-black" role="progressbar" style={{ width: `${tickPercent}%` }} />;
+
     return (
       <div id="bountyEstimator">
-        <table className="progress-markers" style={{ width: '100%' }}>
+        <table className="progress-markers" style={{ width: `${totalWidthPercent}%` }}>
           <tbody>
             <tr>
-              <td className="p-0" style={{ width: '30%' }}>Min</td>
-              <td className="text-center p-0" style={{ width: '20%' }}>Mean</td>
-              <td className="text-right p-0" style={{ width: '50%' }}>Max</td>
+              <td className="p-0" style={{ width: `${redLinePercent}%` }}>Min</td>
+              <td className="text-center p-0" style={{ width: `${yellowLinePercent}%` }}>Mean</td>
+              <td className="text-right p-0" style={{ width: `${greenLinePercent}%` }}>Max</td>
             </tr>
           </tbody>
         </table>
         <div className="bounty-indicator">
           <div className="progress m-2">
-            <div className="progress-bar bg-danger" role="progressbar" style={{ width: '30%' }} />
-            <div className="progress-bar bg-warning" role="progressbar" style={{ width: '20%' }} />
-            <div className="progress-bar bg-success" role="progressbar" style={{ width: '50%' }} />
+            {tick} {redLine} {halfYellowLine} {tick} {halfYellowLine} {greenLine} {tick}
           </div>
         </div>
-        <table className="progress-markers" style={{ width: '100%' }}>
+        <table className="progress-markers" style={{ width: `${totalWidthPercent}%` }}>
           <tbody>
             <tr>
-              <td className="p-0" style={{ width: '30%' }}>{this.props.bountyMin}</td>
-              <td className="text-center p-0" style={{ width: '20%' }}>{this.props.bountyAvg}</td>
-              <td className="text-right p-0" style={{ width: '50%' }}>{this.props.bountyMax}</td>
+              <td className="p-0" style={{ width: `${redLinePercent}%` }}>{this.props.bountyMin}</td>
+                <td className="text-center p-0" style={{ width: `${yellowLinePercent}%` }}>{this.props.bountyAvg}</td>
+              <td className="text-right p-0" style={{ width: `${greenLinePercent}%` }}>{this.props.bountyMax}</td>
             </tr>
           </tbody>
         </table>
