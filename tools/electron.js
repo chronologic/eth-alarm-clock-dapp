72aaea02deb03e711f4348c57843a8ca03944f98
Anthony <adibas03@ymail.com>
2018-05-24 12:13:37 +0100

    set up win build

--- tools/electron.js
+++ tools/electron.js
@@ -1,3 +1,4 @@
+const url = require('url');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
@@ -8,10 +9,44 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({ width: 900, height: 680 });
-  mainWindow.loadURL(
-    isDev ? 'http://localhost:8080' : `file://${path.join(__dirname, '../build/index.html')}`
-  );
+  
+  const WEB_FOLDER = '../out';
+  const PROTOCOL = 'file';
+
+  electron.protocol.interceptFileProtocol(PROTOCOL, (request, callback) => {
+    // // Strip protocol
+    let url = request.url.substr(PROTOCOL.length + 1);
+
+    // Build complete path for node require function
+    url = path.join(__dirname, WEB_FOLDER, url);
+
+    // Replace backslashes by forward slashes (windows)
+    // url = url.replace(/\\/g, '/');
+    url = path.normalize(url);
+
+    console.log(url);
+    callback({path: url});
+  });
+
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false
+    }
+  });
+
+  mainWindow.toggleDevTools();
+  const mainPath =  isDev ? 'http://localhost:8080' : url.format({
+    pathname: 'index.html',
+    protocol: PROTOCOL + ':',
+    slashes: true
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadURL(mainPath);
+
   mainWindow.on('closed', () => (mainWindow = null));
 }
 
