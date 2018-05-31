f3887e50a8fdcc90cf84f920a2744619513119d9
Bagaric <joseph@josephit.com>
2018-05-31 16:31:34 +0200

    Simplified isRunningElectron function

--- app/lib/electron-util.js
+++ app/lib/electron-util.js
@@ -1,10 +1,6 @@
 const isRunningInElectron = () => {
-  // location.search.indexOf('mode=electron') > -1;
   const userAgent = window.navigator.userAgent.toLowerCase();
-  if (userAgent.indexOf(' electron/') > -1) {
-    return true;
-  }
-  return false;
+  return userAgent.indexOf(' electron/') > -1;
 };
 
 export { isRunningInElectron };
