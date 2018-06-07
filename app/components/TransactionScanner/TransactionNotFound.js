8a2ebdfb06570529fd7366d4cd423bb5062291ca
Daniel Kmak <kuzirashi@interia.pl>
2018-06-07 16:55:51 +0200

    Get rid of not needed DOM elements.

--- app/components/TransactionScanner/TransactionNotFound.js
+++ app/components/TransactionScanner/TransactionNotFound.js
@@ -5,20 +5,15 @@ class TransactionNotFound extends Component {
   render() {
     const { address } = this.props;
     return (
-      <div className="">
-        <div className="text-center">
-          <span>
-            <h1 className="error-number">Oops!</h1>
-            <h3 className="semi-bold mt-4">
-              The scheduled transaction <div className="text-grey bold">{address}</div> does not
-              exist.
-            </h3>
+      <div className="text-center">
+        <h1 className="error-number">Oops!</h1>
+        <h3 className="semi-bold mt-4">
+          The scheduled transaction <div className="text-grey bold">{address}</div> does not exist.
+        </h3>
 
-            <div className="pb-4 mt-4">
-              If you feel like the transaction should exist, please try changing the provider
-              you&apos;re using.<br />Alternatively you can refresh the page in some time.
-            </div>
-          </span>
+        <div className="pb-4 mt-4">
+          If you feel like the transaction should exist, please try changing the provider
+          you&apos;re using.<br />Alternatively you can refresh the page in some time.
         </div>
       </div>
     );
