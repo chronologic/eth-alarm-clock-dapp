90ff9adc0134ac7637a0b6b39cbf3b1254cd0c15
Bagaric <joseph@josephit.com>
2018-06-28 11:32:27 +0200

    UI elements

--- app/components/TimeNode/TimeNodeSettings.js
+++ app/components/TimeNode/TimeNodeSettings.js
@@ -19,6 +19,42 @@ class TimeNodeSettings extends Component {
       <div id="timeNodeSettings">
         <div className="card card-transparent">
           <div className="card-header separator">
+            <div className="card-title">Economic Strategy</div>
+          </div>
+          <div className="card-block p-3">
+            <div className="mb-3">
+              You can fine tune which transactions you would like to claim and which you would avoid
+              claiming.<br />
+              <strong>Only claim transactions that</strong>:
+            </div>
+
+            <div className="row vertical-align">
+              <div className="col-md-4">
+                <div className={'form-group form-group-default'}>
+                  <label>Require a deposit lower than</label>
+                  <input className="form-control" type="number" placeholder="Max Deposit in ETH" />
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className={'form-group form-group-default'}>
+                  <label>Bring a profit higher than</label>
+                  <input className="form-control" type="number" placeholder="Profit in ETH" />
+                </div>
+              </div>
+
+              <div className="col-md-4">
+                <div className={'form-group form-group-default'}>
+                  <label>My balance is higher than</label>
+                  <input className="form-control" type="number" placeholder="Balance in ETH" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card card-transparent">
+          <div className="card-header separator">
             <div className="card-title">Detach Wallet</div>
           </div>
           <div className="card-block p-3">
