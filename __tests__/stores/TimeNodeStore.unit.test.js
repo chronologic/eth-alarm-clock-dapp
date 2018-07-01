9526ed964933d5d8955da753b2956d9cfa119059
Bagaric <joseph@josephit.com>
2018-07-01 12:21:35 +0200

    Update tests

--- __tests__/stores/TimeNodeStore.unit.test.js
+++ __tests__/stores/TimeNodeStore.unit.test.js
@@ -1,27 +1,7 @@
 import { ok } from 'assert';
 import TimeNodeStore from '../../app/stores/TimeNodeStore';
-
-class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
-
-  clear() {
-    this.store = {};
-  }
-
-  getItem(key) {
-    return this.store[key] || null;
-  }
-
-  setItem(key, value) {
-    this.store[key] = value.toString();
-  }
-
-  removeItem(key) {
-    delete this.store[key];
-  }
-}
+import LocalStorageMock from '../../__mocks__/LocalStorageMock';
+import { services } from '../../app/services';
 
 describe('Stores / TimeNode', () => {
   describe('getWorkerOptions', () => {
@@ -30,7 +10,7 @@ describe('Stores / TimeNode', () => {
       const KEYSTORE = '{}';
       const KEYSTORE_PASSWORD = 'testtest';
 
-      const timeNodeStore = new TimeNodeStore({}, {});
+      const timeNodeStore = new TimeNodeStore({}, {}, {}, services.storageService);
 
       timeNodeStore.decrypt = encodedValue => {
         if (encodedValue === KEYSTORE_PASSWORD) {
