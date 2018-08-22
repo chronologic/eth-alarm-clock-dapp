1ae72839e6a5eba175d886784225b63af22857fd
Daniel Kmak <kuzirashi@interia.pl>
2018-08-23 00:45:37 +0200

    Remove node_modules from electron-builds.

--- tools/prepareRelease.js
+++ tools/prepareRelease.js
@@ -1,5 +1,9 @@
 const fs = require('fs');
 const paths = require('../webpack/paths');
 const path = require('path');
+const rimraf = require('rimraf');
 
+rimraf.sync(path.join(paths.electronBuilds, 'mac'));
+rimraf.sync(path.join(paths.electronBuilds, 'linux-unpacked'));
+rimraf.sync(path.join(paths.electronBuilds, 'win-unpacked'));
 fs.renameSync(paths.electronBuilds, path.join(paths.dist, 'electron-builds'));
