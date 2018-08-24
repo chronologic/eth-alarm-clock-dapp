866f4390117df775c1d7982d92aa0066ef2a4a19
Daniel Kmak <kuzirashi@interia.pl>
2018-08-24 15:59:44 +0200

    Publish only on tags.

--- tools/test-osx.sh
+++ tools/test-osx.sh
@@ -2,7 +2,5 @@
 
 set -ev # return value 1 (error) if any command fails, and display each command before its run
 if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
-  npm run electron:distribute
-  echo 'electron-builds contains:'
-  ls -lah electron-builds
+  if [ -n "$TRAVIS_TAG" ]; then npm run electron:distribute; fi
 fi
