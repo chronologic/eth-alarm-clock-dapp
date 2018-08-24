5db5c0a2a9b2ddac5c0eae9077c4c9657be92e35
Joseph Bagaric <joseph@josephit.com>
2018-08-24 19:26:31 +0200

    Update master to 0.9.15 (#569)
    * Fix BN error
    
    * toEth
    
    * Electron Icon (#466)
    
    * Add electron icons
    
    * Remove leftovers
    
    * Update version number
    
    * Fix tests on new jest update
    
    * Update version
    
    * Package-json
    
    * Remove win icon
    
    * Win icon electron
    
    * Wrong name
    
    * chore(package): update autoprefixer to version 9.1.2
    
    * fix(package): update mobx to version 5.0.4
    
    * chore(package): update webpack to version 4.17.0
    
    * [FEATURE] Automatic electron builds.
    
    * Rename app to TimeNode.
    
    * Adjust stats to the latest timenode-core changes (#550)
    
    [FEATURE] Reset Stats
    
    * DB name upgrade
    
    * Allow the user to detach the wallet on password screen (#552)
    
    * Add maxGasSubsidy option to TimeNode settings
    
    * adjust to statsdb having init method for loading
    
    * More logs.
    
    * Fix Windows.
    
    * Remove node_modules from electron-builds.
    
    * fix(package): update @ethereum-alarm-clock/timenode-core to version 5.0.2
    
    * Fix copy pasting.
    
    * Enable web security.
    
    * Fix claiming ON/OFF bug + add claiming notification (#559)
    
    * [TimeNode Settings] Unsaved Changes warning (#560)
    
    * Add Unsaved Changes warning to TimeNode Settings
    
    * Add after-save callback
    
    * Fix edge cases
    
    * chore(package): update browserslist to version 4.1.0 (#558)
    
    * chore(package): update react-hot-loader to version 4.3.5 (#557)
    
    * chore(package): update enzyme to version 3.4.4 (#543)
    
    * chore(package): update eac.js-lib to version 2.3.8
    
    * Generate new OSX build.
    
    * [FEATURE] Refactor networks and enable Ropsten (#564)
    
    * Refactor networks and enable Ropsten
    
    * Check if jquery exists on notification
    
    * Fix tests
    
    * [BUGIX] Fix web version routing. (#565)
    
    * [FEATURE] Unsaved changes pop-up (#563)
    
    * Add popup when switching away from unsaved settings
    
    * Remove unnecessary
    
    * Replace confirm() with a proper styled modal
    
    * Typos
    
    * Better name for state
    
    * chore(package): update @ethereum-alarm-clock/timenode-core to version 5.1.0 (#568)
    
    Closes #562
    
    * chore(package): update eac.js-lib to version 2.4.0 (#567)
    
    * [FEATURE] Upload installers to Bintray. (#566)
    
    * 0.9.15

---
+++ electron-app/main/index.js
@@ -0,0 +1,132 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, Menu, protocol } = require('electron');
+const path = require('path');
+const os = require('os');
+
+const isDev = require('electron-is-dev');
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+function createWindow() {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 768
+  });
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html');
+
+  const PROTOCOL = 'file';
+
+  // mainWindow.toggleDevTools();
+
+  const prepareElectron = () => {
+    mainWindow.webContents.once('dom-ready', async () => {
+      await mainWindow.webContents.executeJavaScript('setElectron();');
+    });
+  };
+
+  protocol.interceptFileProtocol(PROTOCOL, (request, callback) => {
+    // Strip protocol
+    let url = request.url.substr(PROTOCOL.length + 1);
+
+    let packagePath = __dirname;
+    const windows = os.platform() === 'win32';
+
+    if (windows) {
+      packagePath = packagePath.replace(/\\/g, '/');
+
+      if (url.indexOf('///') === 0) {
+        url = url.slice(3, url.length);
+      }
+    }
+
+    if (url.indexOf(packagePath) === -1) {
+      // Build complete path for node require function for file paths
+      if (windows) {
+        url = path.join(packagePath, url.slice(3, url.length));
+
+        // Replace backslashes by forward slashes (windows)
+        url = url.replace(/\\/g, '/');
+      } else {
+        url = path.join(packagePath, url);
+      }
+
+      url = path.normalize(url);
+    }
+
+    callback({ path: url });
+
+    if (url.indexOf('index.html') > -1) prepareElectron();
+  });
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+  const template = [
+    {
+      label: 'Application',
+      submenu: [
+        {
+          label: 'Quit',
+          accelerator: 'Command+Q',
+          click: function() {
+            app.quit();
+          }
+        }
+      ]
+    },
+    {
+      label: 'Edit',
+      submenu: [
+        { label: 'Undo', accelerator: 'CmdOrCtrl+Z', selector: 'undo:' },
+        { label: 'Redo', accelerator: 'Shift+CmdOrCtrl+Z', selector: 'redo:' },
+        { type: 'separator' },
+        { label: 'Cut', accelerator: 'CmdOrCtrl+X', selector: 'cut:' },
+        { label: 'Copy', accelerator: 'CmdOrCtrl+C', selector: 'copy:' },
+        { label: 'Paste', accelerator: 'CmdOrCtrl+V', selector: 'paste:' },
+        { label: 'Select All', accelerator: 'CmdOrCtrl+A', selector: 'selectAll:' }
+      ]
+    }
+  ];
+
+  if (!isDev) {
+    Menu.setApplicationMenu(Menu.buildFromTemplate(template));
+  }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function() {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function() {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
