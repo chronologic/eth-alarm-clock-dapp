fdf82f3be84f9822d90a949202db8c3336248fdc
Daniel Kmak <kuzirashi@interia.pl>
2018-08-24 14:56:37 +0200

    Enable win, mac.

--- tools/buildElectron.js
+++ tools/buildElectron.js
@@ -32,8 +32,8 @@ async function buildElectron() {
   console.log('Building...');
   try {
     await builder.build({
-      // mac: shouldBuildOs('mac') ? ['zip', 'dmg'] : undefined,
-      // win: shouldBuildOs('windows') ? ['nsis'] : undefined,
+      mac: shouldBuildOs('mac') ? ['zip', 'dmg'] : undefined,
+      win: shouldBuildOs('windows') ? ['nsis'] : undefined,
       linux: shouldBuildOs('linux') ? ['AppImage'] : undefined,
       x64: true,
       ia32: false,
