f9462ae2994c522ba4c983f9e7efb4536a87be0e
Bagaric <joseph@josephit.com>
2018-08-29 07:25:02 +0200

    Start scanning after restart

--- app/stores/TimeNodeStore.js
+++ app/stores/TimeNodeStore.js
@@ -434,10 +434,11 @@ export default class TimeNodeStore {
     return true;
   }
 
-  restart(password) {
+  async restart(password) {
     this.stopScanning();
     this.eacWorker = null;
     this.startClient(this.walletKeystore, password);
+    await this.startScanning();
   }
 
   resetWallet() {
