152744b613161e4f8a438993a4b7d19d80c09982
Piotr Kosiński <piotrkosinski@gmail.com>
2018-09-05 11:30:24 +0200

    remove unnecesasry checks

--- app/stores/TimeNodeStore.js
+++ app/stores/TimeNodeStore.js
@@ -298,31 +298,13 @@ export default class TimeNodeStore {
       this._keenStore.sendActiveTimeNodeEvent(this.getMyAddress(), this.getAttachedDAYAddress());
     }
   }
-
-  async awaitScanReady() {
-    if (
-      !this.eacWorker ||
-      this.eacWorker === null ||
-      !this._keenStore ||
-      this._keenStore === null
-    ) {
-      return new Promise(resolve => {
-        setTimeout(async () => {
-          resolve(await this.awaitScanReady());
-        }, 500);
-      });
-    }
-    return true;
-  }
-
+ 
   async startScanning() {
     if (this.nodeStatus === TIMENODE_STATUS.DISABLED) {
       return;
     }
 
     this.scanningStarted = true;
-
-    await this.awaitScanReady();
 
     this.sendActiveTimeNodeEvent();
 
