f58293d27443ff492fee080a4db71d7d041d0181
Bagaric <joseph@josephit.com>
2018-09-13 15:56:51 +0800

    Add missing dirname func

--- electron-app/main/index.js
+++ electron-app/main/index.js
@@ -114,6 +114,8 @@ function createWindow() {
 
   globalShortcut.register('F5', mainWindow.reload);
   globalShortcut.register('CommandOrControl+R', mainWindow.reload);
+
+  mainWindow.getDirName = () => __dirname;
 }
 
 // This method will be called when Electron has finished
