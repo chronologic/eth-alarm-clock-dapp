60f034f6cd418b2306c809a17aa8e0d8abd36529
Bagaric <joseph@josephit.com>
2018-09-17 02:02:19 +0800

    Report link

--- app/components/Common/URLNotFound.js
+++ app/components/Common/URLNotFound.js
@@ -9,7 +9,12 @@ class URLNotFound extends Component {
             <span>
               <h1 className="error-number">404</h1>
               <h2 className="semi-bold">Sorry but we couldn&#39;t find this page</h2>
-              <p className="p-b-10">This page you are looking for does not exist. <a href="#">Report this?</a></p>
+              <p className="p-b-10">
+                This page you are looking for does not exist.{' '}
+                <a href="https://github.com/chronologic/eth-alarm-clock-dapp/issues">
+                  Report this?
+                </a>
+              </p>
             </span>
           </div>
         </div>
