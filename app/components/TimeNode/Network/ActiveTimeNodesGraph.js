21544d4f6cf59dc8f1ce012999b0260ac0c6fe37
Bagaric <joseph@josephit.com>
2018-09-24 16:03:23 +0800

    Handle graph updates gracefully

--- app/components/TimeNode/Network/ActiveTimeNodesGraph.js
+++ app/components/TimeNode/Network/ActiveTimeNodesGraph.js
@@ -9,6 +9,11 @@ import moment from 'moment';
 class ActiveTimeNodesGraph extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      chart: null
+    };
+    this.setChart = this.setChart.bind(this);
+    this.updateChart = this.updateChart.bind(this);
     this.refreshChart = this.refreshChart.bind(this);
   }
 
@@ -48,47 +53,73 @@ class ActiveTimeNodesGraph extends Component {
     const sortedValues = Object.values(sortedIntervals);
 
     // Format the values to be displayed on the graph
-    const dataLabels = Object.keys(sortedIntervals).map(
+    const labels = Object.keys(sortedIntervals).map(
       timestamp =>
         moment(parseInt(timestamp))
           .toDate()
           .getHours() + ':00'
     );
-    const dataActiveTns = sortedValues;
 
-    const ctx = this.activeTnsGraph.getContext('2d');
-    new Chart(ctx, {
-      type: 'bar',
-      data: {
-        labels: dataLabels,
-        datasets: [
-          {
-            label: '# of Active TimeNodes',
-            data: dataActiveTns,
-            backgroundColor: 'rgba(33, 255, 255, 0.2)',
-            borderColor: 'rgba(33, 255, 255, 1)',
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [
+    const data = {
+      labels,
+      values: sortedValues
+    };
+
+    if (this.state.chart !== null) {
+      this.updateChart(data);
+    } else {
+      this.setChart(this.activeTnsGraph.getContext('2d'), data);
+    }
+  }
+
+  setChart(ctx, data) {
+    this.setState({
+      chart: new Chart(ctx, {
+        type: 'bar',
+        data: {
+          labels: data.labels,
+          datasets: [
             {
-              ticks: {
-                min: 0,
-                beginAtZero: true,
-                callback: value => {
-                  if (Math.floor(value) === value) {
-                    return value;
+              label: '# of Active TimeNodes',
+              data: data.values,
+              backgroundColor: 'rgba(33, 255, 255, 0.2)',
+              borderColor: 'rgba(33, 255, 255, 1)',
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  min: 0,
+                  beginAtZero: true,
+                  callback: value => {
+                    if (Math.floor(value) === value) {
+                      return value;
+                    }
                   }
                 }
               }
-            }
-          ]
+            ]
+          }
         }
-      }
+      })
     });
+  }
+
+  updateChart(data) {
+    const { chart } = this.state;
+    chart.data.labels.pop();
+    chart.data.datasets.forEach(dataset => {
+      dataset.data.pop();
+    });
+
+    chart.data.labels = data.labels;
+    chart.data.datasets[0].data = data.values;
+
+    chart.update();
   }
 
   compareDates(a, b) {
