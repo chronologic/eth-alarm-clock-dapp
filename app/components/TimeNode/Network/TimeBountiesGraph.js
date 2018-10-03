df3bb55e1b21f7b9c619650bba13025caa828f55
Joseph Bagaric <joseph@josephit.com>
2018-10-03 13:26:35 +0200

    [FEATURE] TimeBounties graph (#740)
    * Initial timebounties graph
    
    * Add stepped line design
    
    * Improve descriptions
    
    * Fill graph with values
    
    * When representing numbers lower than 0.00001 - switch to Gwei instead of ETH
    
    * Typo
    
    * Use TimeNode specific network for time bounties graph
    
    * Fix tests
    
    * Selective web3 provider when getting buckets
    
    * Remove unused function
    
    * Unused function removal #2
    
    * Fix replacing values array error
    
    * 5 min refresh interval

---
+++ app/components/TimeNode/Network/TimeBountiesGraph.js
@@ -0,0 +1,152 @@
+import React, { Component } from 'react';
+import { inject, observer } from 'mobx-react';
+import PropTypes from 'prop-types';
+import { Chart } from 'chart.js';
+import { BeatLoader } from 'react-spinners';
+
+const INITIAL_CURRENCY_DENOMINATION = 'ETH';
+
+@inject('timeNodeStore')
+@inject('web3Service')
+@observer
+class TimeBountiesGraph extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      chart: null,
+      loadingBounties: false,
+      currencyDenomination: INITIAL_CURRENCY_DENOMINATION
+    };
+    this.setChart = this.setChart.bind(this);
+    this.updateChart = this.updateChart.bind(this);
+    this.refreshChart = this.refreshChart.bind(this);
+  }
+
+  componentDidMount() {
+    this.props.onRef(this);
+    this.refreshChart();
+    this.interval = setInterval(this.refreshChart, this.props.refreshInterval);
+  }
+
+  componentWillUnmount() {
+    this.props.onRef(undefined);
+    clearInterval(this.interval);
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.data !== this.props.data) {
+      this.refreshChart();
+    }
+  }
+
+  async refreshChart() {
+    if (!this.state.loadingBounties) {
+      this.setState({ loadingBounties: true });
+    }
+
+    if (this.props.data) {
+      const { web3Service } = this.props;
+      let { labels, values } = this.props.data;
+
+      // Chart.js has issues if all numbers are lower than 0.00001
+      // If that is the case, convert the number representation to GWei
+      if (values.every(value => value < 1e-5)) {
+        const valuesInGwei = [];
+
+        values.forEach(value => {
+          const wei = web3Service.web3.toWei(value, 'ether');
+          valuesInGwei.push(web3Service.web3.fromWei(wei, 'gwei'));
+        });
+        values = valuesInGwei;
+
+        this.setState({ currencyDenomination: 'Gwei' });
+      } else if (this.state.currencyDenomination !== INITIAL_CURRENCY_DENOMINATION) {
+        this.setState({ currencyDenomination: INITIAL_CURRENCY_DENOMINATION });
+      }
+
+      const data = {
+        labels,
+        values
+      };
+
+      if (this.state.chart !== null) {
+        this.updateChart(data);
+      } else {
+        this.setChart(this.timeBountiesGraph.getContext('2d'), data);
+      }
+    }
+
+    this.setState({ loadingBounties: false });
+  }
+
+  setChart(ctx, data) {
+    this.setState({
+      chart: new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: data.labels,
+          datasets: [
+            {
+              label: 'Average TimeBounty',
+              data: data.values,
+              borderColor: 'rgba(33, 255, 255, 1)',
+              borderWidth: 3,
+              fill: false,
+              steppedLine: true
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  min: 0,
+                  beginAtZero: true
+                },
+                scaleLabel: {
+                  display: true,
+                  labelString: this.state.currencyDenomination
+                }
+              }
+            ]
+          },
+          legend: {
+            display: false
+          }
+        }
+      })
+    });
+  }
+
+  updateChart(data) {
+    const { chart } = this.state;
+    chart.data.labels.pop();
+    chart.data.datasets.forEach(dataset => {
+      dataset.data.pop();
+    });
+
+    chart.data.labels = data.labels;
+    chart.data.datasets[0].data = data.values;
+
+    chart.update();
+  }
+
+  render() {
+    return (
+      <div id="timeBountiesGraphWrapper" className="horizontal-center">
+        {(this.state.loadingBounties || !this.props.data) && <BeatLoader />}
+        <canvas id="timeBountiesGraph" ref={el => (this.timeBountiesGraph = el)} />
+      </div>
+    );
+  }
+}
+
+TimeBountiesGraph.propTypes = {
+  onRef: PropTypes.any,
+  data: PropTypes.any,
+  web3Service: PropTypes.any,
+  refreshInterval: PropTypes.number
+};
+
+export { TimeBountiesGraph };
--- app/components/TimeNode/TimeNodeNetwork.js
+++ app/components/TimeNode/TimeNodeNetwork.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { inject, observer } from 'mobx-react';
-import { ActiveTimeNodesGraph } from './Network';
+import { ActiveTimeNodesGraph, TimeBountiesGraph } from './Network';
 
 // 2 min
-const REFRESH_INTERVAL = 120 * 1000;
+const TWO_MIN = 120 * 1000;
+const TEN_MIN = 10 * 60 * 1000;
 
 @inject('keenStore')
+@inject('timeNodeStore')
 @observer
 class TimeNodeNetwork extends Component {
   render() {
@@ -16,10 +18,7 @@ class TimeNodeNetwork extends Component {
       latestActiveTimeNodes.length > 0 || Object.keys(historyActiveTimeNodes).length > 0;
 
     const activeTnsGraph = shouldShowActiveTnsGraph ? (
-      <ActiveTimeNodesGraph
-        onRef={ref => (this.activeTnsGraph = ref)}
-        refreshInterval={REFRESH_INTERVAL}
-      />
+      <ActiveTimeNodesGraph onRef={ref => (this.activeTnsGraph = ref)} refreshInterval={TWO_MIN} />
     ) : (
       <p>No data yet.</p>
     );
@@ -50,6 +49,34 @@ class TimeNodeNetwork extends Component {
               <div className="card-body">{activeTnsGraph}</div>
             </div>
           </div>
+
+          <div className="col-md-6">
+            <div data-pages="card" className="card card-default">
+              <div className="card-header">
+                <div className="card-title">Average TimeBounty (last 24h)</div>
+                <div className="card-controls">
+                  <ul>
+                    <li>
+                      <a
+                        data-toggle="refresh"
+                        className="card-refresh"
+                        onClick={() => this.timeBountiesGraph.refreshChart()}
+                      >
+                        <i className="card-icon card-icon-refresh" />
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card-body">
+                <TimeBountiesGraph
+                  onRef={ref => (this.timeBountiesGraph = ref)}
+                  data={this.props.timeNodeStore.bountiesGraphData}
+                  refreshInterval={TEN_MIN}
+                />
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     );
@@ -57,7 +84,8 @@ class TimeNodeNetwork extends Component {
 }
 
 TimeNodeNetwork.propTypes = {
-  keenStore: PropTypes.any
+  keenStore: PropTypes.any,
+  timeNodeStore: PropTypes.any
 };
 
 export default TimeNodeNetwork;
