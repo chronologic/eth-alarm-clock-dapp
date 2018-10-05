e6ebe43d5d11bc74723d1f1db5b1d0f9e8696188
Joseph Bagaric <joseph@josephit.com>
2018-10-05 10:30:20 +0200

    [FEATURE] Processed Transactions Graph (#767)
    * Initial processed transactions graph
    
    * Check if currently updating

--- app/components/TimeNode/Network/index.js
+++ app/components/TimeNode/Network/index.js
@@ -1,2 +1,3 @@
 export { ActiveTimeNodesGraph } from './ActiveTimeNodesGraph';
 export { TimeBountiesGraph } from './TimeBountiesGraph';
+export { ProcessedTransactionsGraph } from './ProcessedTransactionsGraph';
